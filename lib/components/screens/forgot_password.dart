297bbc8617f5a4c94b7ab9f0beeb72dc88c142db
Kennedy Lino Gyimah <linogyimahkennedy@gmail.com>
2023-08-31 05:50:39 +0100

    Added the app bar to the forgot password screen

--- lib/components/screens/forgot_password.dart
+++ lib/components/screens/forgot_password.dart
@@ -14,80 +14,93 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                margin: const EdgeInsets.only(left: 16.0, right: 16.0),
-                padding: const EdgeInsets.only(left: 20.0),
-                width: 343.0,
-                height: 56.0,
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20.0)),
-                alignment: Alignment.centerLeft,
-                child: const Text(
-                  'New Password',
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                    fontFamily: 'OpenSans',
-                    color: Color(0xFF212121),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 16.0,
-              ),
-              Container(
-                margin: const EdgeInsets.only(left: 16.0, right: 16.0),
-                padding: const EdgeInsets.only(left: 20.0),
-                width: 343.0,
-                height: 56.0,
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20.0)),
-                alignment: Alignment.centerLeft,
-                child: const Text(
-                  'Confirm New Password',
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                    fontFamily: 'OpenSans',
-                    color: Color(0xFF212121),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 68.0,
-              ),
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const HomeScreen()),
-                  );
-                },
-                child: Container(
-                  margin: const EdgeInsets.only(left: 16.0, right: 16.0),
-                  width: 343.0,
-                  height: 56.0,
-                  decoration: BoxDecoration(
-                      color: const Color(0xFFE6B014),
-                      borderRadius: BorderRadius.circular(20.0)),
-                  alignment: Alignment.center,
-                  child: const Text(
-                    'Reset Password',
-                    style: TextStyle(
-                      fontFamily: 'OpenSans',
-                      color: Color(0xFF212121),
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-            ],
+      appBar: AppBar(
+        flexibleSpace: SafeArea(
+          child: Container(
+            width: 81.03,
+            height: 32.0,
+            margin: const EdgeInsets.only(right: 16.0, top: 16.0, bottom: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Image.asset('assets/images/app_icon.png'),
+              ],
+            ),
           ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(left: 16.0, right: 16.0),
+              padding: const EdgeInsets.only(left: 20.0),
+              width: 343.0,
+              height: 56.0,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20.0)),
+              alignment: Alignment.centerLeft,
+              child: const Text(
+                'New Password',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontFamily: 'OpenSans',
+                  color: Color(0xFF212121),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 16.0,
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 16.0, right: 16.0),
+              padding: const EdgeInsets.only(left: 20.0),
+              width: 343.0,
+              height: 56.0,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20.0)),
+              alignment: Alignment.centerLeft,
+              child: const Text(
+                'Confirm New Password',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontFamily: 'OpenSans',
+                  color: Color(0xFF212121),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 68.0,
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomeScreen()),
+                );
+              },
+              child: Container(
+                margin: const EdgeInsets.only(left: 16.0, right: 16.0),
+                width: 343.0,
+                height: 56.0,
+                decoration: BoxDecoration(
+                    color: const Color(0xFFE6B014),
+                    borderRadius: BorderRadius.circular(20.0)),
+                alignment: Alignment.center,
+                child: const Text(
+                  'Reset Password',
+                  style: TextStyle(
+                    fontFamily: 'OpenSans',
+                    color: Color(0xFF212121),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
